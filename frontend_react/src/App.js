780d938f1667dd078c822b12d807c6b9ee60667e
dhimzikri <dschok182@gmail.com>
2022-09-16 15:11:04 +0700

    addSomeAssets

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import { About, Footer, Header, Skills, Testimonials, Work } from './container';
 import Navbar from './components/Navbar/Navbar'
 import './App.scss'
 
 const App = () => {
   return (
-    <div className='app'>
+    <div className='app' >
       <Navbar />
       <Header />
       <About />
--- frontend_react/src/constants/image.js
+++ frontend_react/src/constants/image.js
@@ -35,6 +35,16 @@ import bolt from '../assets/bolt.png';
 import nb from '../assets/nb.png';
 import skype from '../assets/skype.png';
 import spotify from '../assets/spotify.png';
+import img1 from '../assets/1.png';
+import img2 from '../assets/3.webp';
+import img3 from '../assets/4.png';
+import img4 from '../assets/5.png';
+import img5 from '../assets/6.png';
+import img6 from '../assets/7.png';
+import img7 from '../assets/8.jpg';
+import img8 from '../assets/9.png';
+import img9 from '../assets/10.png';
+import img10 from '../assets/11.png';
 
 export default {
     email,
@@ -70,5 +80,15 @@ export default {
     nb,
     skype,
     spotify,
-    profile1
+    profile1,
+    img1,
+img2,
+img3,
+img4,
+img5,
+img6,
+img7,
+img8,
+img9,
+img10,
 };
